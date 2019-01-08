84f1e09517db49b53ade0bdc27b3c9b7de06afaf
PythonRobak <adrian@cgmasters.pl>
2019-01-08 12:24:34 +0100

    004 - working on exam sheets models

--- exam_sheets/models.py
+++ exam_sheets/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class ExamSheet(models.Model):
+
+    exam_sheet_title = models.CharField(max_length=50, blank=False)
+    task_01_title = models.CharField(max_length=50, blank=False)
+    task_01_description = models.TextField(blank=False)
+    task_01_max_points = models.IntegerField(blank=False)
+
+    task_02_title = models.CharField(max_length=50, blank=False)
+    task_02_description = models.TextField(blank=False)
+    task_02_max_points = models.IntegerField(blank=False)
+
+    task_03_title = models.CharField(max_length=50, blank=False)
+    task_03_description = models.TextField(blank=False)
+    task_03_max_points = models.IntegerField(blank=False)
+
+    task_04_title = models.CharField(max_length=50, blank=False)
+    task_04_description = models.TextField(blank=False)
+    task_04_max_points = models.IntegerField(blank=False)
+
+    task_05_title = models.CharField(max_length=50, blank=False)
+    task_05_description = models.TextField(blank=False)
+    task_05_max_points = models.IntegerField(blank=False)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class CompletedExaminationSheet(models.Model):
+
+    answer_01 = models.TextField()
+    answer_01_points_earned = models.IntegerField(blank=True)
+    answer_02 = models.TextField()
+    answer_02_points_earned = models.IntegerField(blank=True)
+    answer_03 = models.TextField()
+    answer_03_points_earned = models.IntegerField(blank=True)
+    answer_04 = models.TextField()
+    answer_04_points_earned = models.IntegerField(blank=True)
+    answer_05 = models.TextField()
+    answer_05_points_earned = models.IntegerField(blank=True)
+
+    entrant = models.ForeignKey(User, on_delete=models.CASCADE)
+    average_score = models.FloatField(blank=True)
+    final_rating = models.IntegerField(blank=True)
+    checked_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+
