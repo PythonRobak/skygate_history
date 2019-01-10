5bfd2984281f70e7cc9ddba79f1f4afca31ae77a
PythonRobak <adrian@cgmasters.pl>
2019-01-11 00:53:37 +0100

    extending abstract user

--- exam_sheets/models.py
+++ exam_sheets/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 
-
+class CustomUser(AbstractBaseUser)
 
 
 class ExamSheet(models.Model):
